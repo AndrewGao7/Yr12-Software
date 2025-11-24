56a4b7cc6710e11737186ecd4f09bb1914536821
AndrewGao7 <andrew.gao7@education.nsw.gov.au>
2025-11-24 11:35:58 +1100

    Creating files for AT1

--- PWA with secure login/app.py
+++ PWA with secure login/app.py
@@ -7,7 +7,38 @@ app.secret_key  = os.urandom(24)
 
 @app.route('/')
 def login():
-    return "hello world"
+    return render_template('login.html')
 
 if __name__ == '__name__':
     app.run(debug=True)
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    connection =sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+
+    user = cursor.execute("SELECT * FROM USER WHERE email=? AND password=?", (email,password)).fetchall()
+    if len(user) > 0:
+        return redirect(f'/home?fname={user[0][0]}&lname={user[0][1]}&email={user[0][2]}')
+    else:
+        return redirect('/')
+    
+@app.route('/home')
+def home():
+    fname = request.args.get('fname')
+    lname = request.args.get('lname')
+    email = request.args.get('email')
+
+    return render_template('home.html', fname=fname, lname=lname, email=email)
+
+@app.route('/signUp')
+def signUp():
+    return render_template('signUp.html')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    fname = request.form.get('fname')
+    
